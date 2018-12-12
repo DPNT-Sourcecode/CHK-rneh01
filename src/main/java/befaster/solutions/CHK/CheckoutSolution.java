f5446870d96c9b13f123b1ddcb8b5a73e1aa2020
sbx_user1059 <sbx_user1059@ip-10-128-45-10.eu-west-2.compute.internal>
2018-12-12 23:19:30 +0000

    Wed Dec 12 23:19:30 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -34,16 +34,17 @@ public class CheckoutSolution {
             }
         }
 
-        total += AAA_PRICE * aCounter / 3;
-        total += A_PRICE * aCounter % 3;
-        total += BB_PRICE * bCounter / 2;
-        total += B_PRICE * bCounter % 2;
+        total += AAA_PRICE * (aCounter / 3);
+        total += A_PRICE * (aCounter % 3);
+        total += BB_PRICE * (bCounter / 2);
+        total += B_PRICE * (bCounter % 2);
         total += C_PRICE * cCounter;
         total += D_PRICE * dCounter;
 
         return total;
     }
 }
+
 
 
 
