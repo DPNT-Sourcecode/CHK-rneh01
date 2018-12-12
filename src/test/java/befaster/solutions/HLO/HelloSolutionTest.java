1f19e4c76a91c855f4c775f43e1d6b3611bd9b86
sbx_user1059 <sbx_user1059@ip-10-128-45-10.eu-west-2.compute.internal>
2018-12-12 22:33:22 +0000

    Wed Dec 12 22:33:22 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,22 @@
 package befaster.solutions.HLO;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class HelloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setUp() {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void testHello() {
+        assertThat(helloSolution.hello(""), equalTo("Hello"));
+    }
 }
+
