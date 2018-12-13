d4897eb350206e36da2036c70ddab91276c8c0c7
sbx_user1059 <sbx_user1059@ip-10-128-45-10.eu-west-2.compute.internal>
2018-12-13 00:35:30 +0000

    Thu Dec 13 00:35:30 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,10 +5,19 @@ import java.util.Map;
 
 public class CheckoutSolution {
     private final Map<String, Integer> INDIVIDUAL_PRICES = new HashMap<>();
+    private final Map<String, Integer> counters = new HashMap<>();
 
     private final int AAA_PRICE = 130;
     private final int AAAAA_PRICE = 200;
     private final int BB_PRICE = 45;
+    private final int FIVE_H_PRICE = 45;
+    private final int TEN_H_PRICE = 80;
+    private final int TWO_K_PRICE = 150;
+    private final int FIVE_P_PRICE = 200;
+    private final int THREE_Q_PRICE = 80;
+    private final int TWO_V_PRICE = 90;
+    private final int THREE_V_PRICE = 130;
+
 
     public CheckoutSolution() {
         INDIVIDUAL_PRICES.put("A", 50);
@@ -18,6 +27,33 @@ public class CheckoutSolution {
         INDIVIDUAL_PRICES.put("E", 40);
         INDIVIDUAL_PRICES.put("F", 10);
         INDIVIDUAL_PRICES.put("G", 20);
+        INDIVIDUAL_PRICES.put("H", 10);
+        INDIVIDUAL_PRICES.put("I", 35);
+        INDIVIDUAL_PRICES.put("J", 60);
+        INDIVIDUAL_PRICES.put("K", 80);
+        INDIVIDUAL_PRICES.put("L", 90);
+        INDIVIDUAL_PRICES.put("M", 15);
+        INDIVIDUAL_PRICES.put("N", 40);
+        INDIVIDUAL_PRICES.put("O", 10);
+        INDIVIDUAL_PRICES.put("P", 50);
+        INDIVIDUAL_PRICES.put("Q", 30);
+        INDIVIDUAL_PRICES.put("R", 50);
+        INDIVIDUAL_PRICES.put("S", 30);
+        INDIVIDUAL_PRICES.put("T", 20);
+        INDIVIDUAL_PRICES.put("U", 40);
+        INDIVIDUAL_PRICES.put("V", 50);
+        INDIVIDUAL_PRICES.put("W", 20);
+        INDIVIDUAL_PRICES.put("X", 90);
+        INDIVIDUAL_PRICES.put("Y", 10);
+        INDIVIDUAL_PRICES.put("Z", 50);
+
+        counters.put("A", 0);
+        counters.put("B", 30);
+        counters.put("C", 20);
+        counters.put("D", 15);
+        counters.put("E", 40);
+        counters.put("F", 10);
+        counters.put("G", 20);
         INDIVIDUAL_PRICES.put("H", 10);
         INDIVIDUAL_PRICES.put("I", 35);
         INDIVIDUAL_PRICES.put("J", 60);
@@ -96,3 +132,4 @@ public class CheckoutSolution {
         return total;
     }
 }
+
