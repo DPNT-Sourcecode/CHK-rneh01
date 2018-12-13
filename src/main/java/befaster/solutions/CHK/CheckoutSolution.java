4db5e181809370f0a56ed021843ea95d7f4b8e08
sbx_user1059 <sbx_user1059@ip-10-128-45-10.eu-west-2.compute.internal>
2018-12-13 00:29:30 +0000

    Thu Dec 13 00:29:30 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,15 +1,43 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
-    private final int A_PRICE = 50;
-    private final int B_PRICE = 30;
-    private final int C_PRICE = 20;
-    private final int D_PRICE = 15;
-    private final int E_PRICE = 40;
-    private final int F_PRICE = 10;
+    private final Map<String, Integer> INDIVIDUAL_PRICES = new HashMap<>();
+
     private final int AAA_PRICE = 130;
     private final int AAAAA_PRICE = 200;
     private final int BB_PRICE = 45;
+
+    public CheckoutSolution() {
+        INDIVIDUAL_PRICES.put("A", 50);
+        INDIVIDUAL_PRICES.put("B", 30);
+        INDIVIDUAL_PRICES.put("C", 20);
+        INDIVIDUAL_PRICES.put("D", 15);
+        INDIVIDUAL_PRICES.put("E", 40);
+        INDIVIDUAL_PRICES.put("F", 10);
+        INDIVIDUAL_PRICES.put("G", 20);
+        INDIVIDUAL_PRICES.put("H", 10);
+        INDIVIDUAL_PRICES.put("I", 35);
+        INDIVIDUAL_PRICES.put("J", 60);
+        INDIVIDUAL_PRICES.put("K", 80);
+        INDIVIDUAL_PRICES.put("L", 90);
+        INDIVIDUAL_PRICES.put("M", 15);
+        INDIVIDUAL_PRICES.put("N", 40);
+        INDIVIDUAL_PRICES.put("O", 10);
+        INDIVIDUAL_PRICES.put("P", 50);
+        INDIVIDUAL_PRICES.put("Q", 30);
+        INDIVIDUAL_PRICES.put("R", 50);
+        INDIVIDUAL_PRICES.put("S", 30);
+        INDIVIDUAL_PRICES.put("T", 20);
+        INDIVIDUAL_PRICES.put("U", 40);
+        INDIVIDUAL_PRICES.put("V", 50);
+        INDIVIDUAL_PRICES.put("W", 20);
+        INDIVIDUAL_PRICES.put("X", 90);
+        INDIVIDUAL_PRICES.put("Y", 10);
+        INDIVIDUAL_PRICES.put("Z", 50);
+    }
 
     public Integer checkout(String skus) {
         int aCounter = 0;
@@ -48,22 +76,22 @@ public class CheckoutSolution {
         total += AAAAA_PRICE * (aCounter / 5);
         aCounter = aCounter % 5;
         total += AAA_PRICE * (aCounter / 3);
-        total += A_PRICE * (aCounter % 3);
+        total += INDIVIDUAL_PRICES.get("A") * (aCounter % 3);
 
         bCounter -= eCounter / 2;
         if (bCounter > 0) {
             total += BB_PRICE * (bCounter / 2);
-            total += B_PRICE * (bCounter % 2);
+            total += INDIVIDUAL_PRICES.get("B") * (bCounter % 2);
         }
 
-        total += C_PRICE * cCounter;
+        total += INDIVIDUAL_PRICES.get("C") * cCounter;
 
-        total += D_PRICE * dCounter;
+        total += INDIVIDUAL_PRICES.get("D") * dCounter;
 
-        total += E_PRICE * eCounter;
+        total += INDIVIDUAL_PRICES.get("E") * eCounter;
 
         fCounter -= fCounter / 3;
-        total += F_PRICE * fCounter;
+        total += INDIVIDUAL_PRICES.get("F") * fCounter;
 
         return total;
     }
